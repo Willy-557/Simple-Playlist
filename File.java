bbcff6e0c94189ccff839498c4e1cbb2baccfa0a
Willy-557 <williamfsetiadi@gmail.com>
2025-12-23 18:19:32 +0700

    Make the menu of spotify mini

---
+++ File.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.util.HashMap;
+import java.util.ArrayList;
+
+public class File {
+    public static void main(String[] args) {
+        HashMap<String, ArrayList<String>> spotifyMini = new HashMap<>();
+        Scanner scanner = new Scanner (System.in);
+
+        while (true) {
+            System.out.println("=== SPOTIFY MINI ===");
+            System.out.println("1. Buat Playlist Baru");
+            System.out.println("2. Tambah Lagu ke Playlist");
+            System.out.println("3. Putar Playlist");
+            System.out.println("4. Keluar");
+
+            
+        }
+
+    }
+}
